1098aadea115b7b388641828acc3e62bc6155973
akashmishra <aakashmishraam5@gmail.com>
2023-09-15 21:44:41 +0530

    spring thymeleaf

---
+++ src/test/java/com/akash/springboot/thyleafdemo/ThyleafdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akash.springboot.thyleafdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThyleafdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
